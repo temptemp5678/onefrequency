b98b925cb54bd401cff54ed651cc8988b083e7a9
DongFlexxia <dong@flexxia.ca>
2016-03-06 16:15:52 -0500

    clean code

--- sites/all/themes/fxt/template.php
+++ sites/all/themes/fxt/template.php
@@ -4,3 +4,18 @@
  * @file
  * template.php
  */
+
+/** - - - - - - base_path() - - - - - - - - - - - - - - - - - - - -  */
+/**
+ * pass "base path" to JS
+ */
+drupal_add_js('jQuery.extend(Drupal.settings, { "basePath": "' . base_path() . '" });', 'inline');
+
+/** - - - - - - current language url prefix - - - - - - - - - - - - - - - - - - - -  */
+global $language;
+if (isset($language->language)) {
+  if ($language->language == 'zh-hans') {
+    drupal_add_js('jQuery.extend(Drupal.settings, { "currentLanguagePrefix": "' . $language->language . '" });', 'inline');
+  }
+}
+
